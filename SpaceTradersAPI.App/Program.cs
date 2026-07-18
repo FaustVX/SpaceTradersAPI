eed2c040a61164c15c0ded76b8317c8f28420e1e
FaustVX <jodu035@gmail.com>
2026-07-18 17:25:18 +0200

    Started CLI

--- SpaceTradersAPI.App/Program.cs
+++ SpaceTradersAPI.App/Program.cs
@@ -5,29 +5,175 @@ using SpaceTradersAPI.Lib.Responses;
 
 var accounts = await ReadAccounts(ReadFile());
 
-if (accounts.Selected.Agents is [])
+while(true)
 {
-    var registration = await accounts.Selected.API.RegisterAgent("FAUSTVX", V2.FactionSymbol.Galactic).ValueOrThrowAsync();
-    Console.WriteLine(registration.Agent.HeadQuarters);
-    accounts.Selected.SelectedAgent = accounts.Selected.Agents[0];
-    var ship = await accounts.Selected.SelectedAgent.API.GetShip("FAUSTVX-2").ValueOrThrowAsync();
-    Console.WriteLine(ship);
-    var shipPos = await ship.Nav.WaypointSymbol.GetWaypointData().ValueOrThrowAsync();
-    (V2.Waypoint, long) closerShipyard = (default!, long.MaxValue);
-    await foreach (var loc in accounts.API.ListWaypointInSystem(ship.Nav.WaypointSymbol.System, traits: [V2.WaypointTraitSymbol.Shipyard]))
-        if (loc.DistanceWithSquared(shipPos) is var dist && dist < closerShipyard.Item2)
-            closerShipyard = (loc, dist);
-    Console.WriteLine(closerShipyard);
-    await ship.Orbit();
-    await ship.Navigate(closerShipyard.Item1.Symbol);
+    switch (Console.Prompt("SpaceTrader > ").Split(' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+    {
+        case [] or ["--help"]:
+            Console.WriteLine("[sel[ect] | list | quit | --help]");
+            break;
+        case ["sel" or "select", .. var selection]:
+            ParseSelect(selection, ["sel[ect]"]);
+            break;
+        case ["list", .. var selection]:
+            ParseList(selection, ["list"]);
+            break;
+        case ["quit"]:
+            Environment.Exit(0);
+            break;
+    }
 }
+
+void ParseSelect(ReadOnlySpan<string> selection, ReadOnlySpan<string> previousCommands)
 {
-    var ship = accounts.Selected.SelectedAgent.SelectedShip;
-    // await ship.PatchNav(V2.ShipNavFlightMode.Drift);
-    Console.WriteLine(ship);
-    await foreach (var waypoint in accounts.API.ListWaypointInSystem(ship.Nav.WaypointSymbol.System, V2.WaypointType.Moon, [V2.WaypointTraitSymbol.Frozen, V2.WaypointTraitSymbol.Marketplace]))
-        Console.WriteLine($"{waypoint}: {ship.CalculateTravelCost(waypoint)}");
-    await ship.Nav.Await();
+    switch (selection)
+    {
+        case [] or ["--help"]:
+            Console.WriteLine($"{previousCommands.Concat()} [account | agent | ship | --help]");
+            break;
+        case ["account", .. var account]:
+            ParseAccount(account, [..previousCommands, "account"]);
+            break;
+        case ["agent", .. var agent]:
+            ParseAgent(agent, [..previousCommands, "agent"]);
+            break;
+        case ["ship", .. var ship]:
+            ParseShip(ship, [..previousCommands, "ship"]);
+            break;
+    }
+
+    void ParseAccount(ReadOnlySpan<string> account, ReadOnlySpan<string> previousCommands)
+    {
+        switch (account)
+        {
+            case [] or ["--help"]:
+                Console.WriteLine($"{previousCommands.Concat()} [<accountName> | --help]");
+                break;
+            case [var name]:
+                accounts.Selected = AssignAccount(accounts, name);
+                break;
+        }
+
+        static AccountItem AssignAccount(Account accounts, string name)
+        {
+            foreach (var a in accounts.Accounts)
+            {
+                if (a.Name == name)
+                    return a;
+            }
+            (Console.ForegroundColor, var fore) = (ConsoleColor.Red, Console.ForegroundColor);
+            Console.WriteLine($"Unknown account <{name}>");
+            return accounts.Selected;
+        }
+    }
+
+    void ParseAgent(ReadOnlySpan<string> agent, ReadOnlySpan<string> previousCommands)
+    {
+        switch (agent)
+        {
+            case [] or ["--help"]:
+                Console.WriteLine($"{previousCommands.Concat()} [<agentName> | --help]");
+                break;
+            case [var name]:
+                accounts.Selected.SelectedAgent = AssignAgent(accounts, name);
+                break;
+        }
+
+        static AccountAgent AssignAgent(Account accounts, string name)
+        {
+            foreach (var a in accounts.Selected.Agents)
+            {
+                if (a.Name == name)
+                    return a;
+            }
+            (Console.ForegroundColor, var fore) = (ConsoleColor.Red, Console.ForegroundColor);
+            Console.WriteLine($"Unknown agent <{name}>");
+            return accounts.Selected.SelectedAgent;
+        }
+    }
+
+    void ParseShip(ReadOnlySpan<string> ship, ReadOnlySpan<string> previousCommands)
+    {
+        switch (ship)
+        {
+            case [] or ["--help"]:
+                Console.WriteLine($"{previousCommands.Concat()} [<shipName> | --help]");
+                break;
+            case [var name]:
+                accounts.Selected.SelectedAgent.SelectedShip = AssignShip(accounts, name);
+                break;
+        }
+
+        static V2.Ship AssignShip(Account accounts, string name)
+        {
+            if (accounts.Selected.SelectedAgent.Ships.TryGetValue(name, out var ship))
+                return ship;
+            (Console.ForegroundColor, var fore) = (ConsoleColor.Red, Console.ForegroundColor);
+            Console.WriteLine($"Unknown ship <{name}>");
+            return accounts.Selected.SelectedAgent.SelectedShip;
+        }
+    }
+}
+
+void ParseList(ReadOnlySpan<string> selection, ReadOnlySpan<string> previousCommands)
+{
+    switch (selection)
+    {
+        case [] or ["--help"]:
+            Console.WriteLine($"{previousCommands.Concat()} [account | agent | ship | --help]");
+            break;
+        case ["account", .. var account]:
+            ParseAccount(account, [..previousCommands, "account"]);
+            break;
+        case ["agent", .. var agent]:
+            ParseAgent(agent, [..previousCommands, "agent"]);
+            break;
+        case ["ship", .. var ship]:
+            ParseShip(ship, [..previousCommands, "ship"]);
+            break;
+    }
+
+    void ParseAccount(ReadOnlySpan<string> account, ReadOnlySpan<string> previousCommands)
+    {
+        switch (account)
+        {
+            case ["--help"]:
+                Console.WriteLine($"{previousCommands.Concat()} [--help]");
+                break;
+            case []:
+                foreach (var a in accounts.Accounts)
+                    Console.WriteLine($"Account: {(a == accounts.Selected ? '*' : ' ')}{a.Name}");
+                break;
+        }
+    }
+
+    void ParseAgent(ReadOnlySpan<string> agent, ReadOnlySpan<string> previousCommands)
+    {
+        switch (agent)
+        {
+            case ["--help"]:
+                Console.WriteLine($"{previousCommands.Concat()} [--help]");
+                break;
+            case []:
+                foreach (var a in accounts.Selected.Agents)
+                    Console.WriteLine($"Agent: {(a == accounts.Selected.SelectedAgent ? '*' : ' ')}{a.Name}");
+                break;
+        }
+    }
+
+    void ParseShip(ReadOnlySpan<string> ship, ReadOnlySpan<string> previousCommands)
+    {
+        switch (ship)
+        {
+            case ["--help"]:
+                Console.WriteLine($"{previousCommands.Concat()} [--help]");
+                break;
+            case []:
+                foreach (var a in accounts.Selected.SelectedAgent.Ships)
+                    Console.WriteLine($"Ship: {(a.Value == accounts.Selected.SelectedAgent.SelectedShip ? '*' : ' ')}{a.Key}");
+                break;
+        }
+    }
 }
 
 static async Task<Account> ReadAccounts(FileInfo accountsFile)
@@ -73,4 +219,22 @@ static FileInfo ReadFile()
         Environment.Exit(-1);
     }
     return accountsFile;
+}
+
+static class Ext
+{
+    extension(ReadOnlySpan<string> strings)
+    {
+        public string Concat()
+        => string.Join(' ', strings);
+    }
+
+    extension(Console)
+    {
+        public static string Prompt(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine()!;
+        }
+    }
 }
